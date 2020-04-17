e08b6e913b5dfbabde8d0cdbc2082e22dd2aade9
Charlies Yacniel <charliesyacniel@gmail.com>
2020-04-16 23:37:06 -0600

    cambios de estructura de carpetas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  // { path: '', redirectTo: '/', pathMatch: 'full' },
+  // { path: '/', component: LoginComponent },
+  {
+    path: '',
+    component: HomeComponent,
+    // children: [
+    //   {
+    //       // path: '',
+    //       // loadChildren: 'pages/home/home.module#HomeModule'
+    //   },
+    //  ]
+  }
+];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
